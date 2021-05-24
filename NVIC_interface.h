3ed573af504430e87d3d1a6feae6db2bc364f9cb
fatma279 <60367290+fatma279@users.noreply.github.com>
2021-05-25 00:00:39 +0200

    -

--- NVIC_interface.h
+++ NVIC_interface.h
@@ -6,13 +6,7 @@
 #define NVIC_INTERFACE_H
 
 
-                 /*GroupPriority*/
-				 
-#define GROUP4          0x05fa0300          /* 4 bits for Group & 0 bits for Sub Groub */
-#define GROUP3          0x05fa0400          /* 3 bits for Group & 1 bits for Sub Groub */
-#define GROUP2          0x05fa0500          /* 2 bits for Group & 2 bits for Sub Groub */
-#define GROUP1          0x05fa0600          /* 1 bits for Group & 3 bits for Sub Groub */
-#define GROUP0          0x05fa0700          /* 0 bits for Group & 4 bits for Sub Groub */
+             
 
 void NVIC_voidEnableInterrupt  (u8 Copy_u8IntNumber);
 void NVIC_voidDisableInterrupt (u8 Copy_u8IntNumber);
